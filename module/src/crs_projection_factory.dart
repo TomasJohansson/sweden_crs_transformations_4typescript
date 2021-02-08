cc259e6c9c2f8a5d2b0a1fdb0d7eae0c9a7587bd
Tomas Johansson <github-tomas@programmerare.com>
2021-02-08 02:18:57 +0100

    Deleted two files, since the code in them was moved to other files in the previous commit.

--- module/src/crs_projection_factory.dart
+++
@@ -1,4 +0,0 @@
-﻿// TODO delete this file since its two methods 
-// getCrsProjectionByEpsgNumber and getAllCrsProjections
-// are now (in this TypeScript implementation) located in CrsProjection
-// which is a class in TypeScript (but is an enum in Dart)
--- module/test/crs_projection_factory_test.dart
+++
@@ -1,4 +0,0 @@
-﻿// TODO delete this file since its tests 
-// has now (in the TypeScript implementation) has moved (and been modified to TypeScript) into the file 'crs_projection.test.ts'
-
-
